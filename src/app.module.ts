71e0432d981f6610720a1e47f74aeec1e0d43a3e
jarray-kais <jarraykais1@gmail.com>
2024-10-05 00:09:40 +0100

    c
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 
 
 @Module({
@@ -11,5 +13,7 @@ import { ConfigModule } from '@nestjs/config';
     MongooseModule.forRoot(process.env.MONGO_URI),
     AuthModule,
   ],
+  controllers: [AppController],
+  providers: [AppService]
 })
 export class AppModule {}
