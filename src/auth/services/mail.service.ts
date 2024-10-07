e2bc37bfd27724527f68bd305f43330b0025455a
jarray-kais <jarraykais1@gmail.com>
2024-10-07 06:21:21 +0100

    Create an EmailService to send emails using Nodemailer.
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

---
+++ src/auth/services/mail.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, Logger } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+
+@Injectable()
+export class EmailService {
+  private transporter: nodemailer.Transporter;
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail', // your email provider
+      auth: {
+        user: process.env.NODEJS_GMAIL_APP_USER, // your email address
+        pass: process.env.NODEJS_GMAIL_APP_PASSWORD, // your email password
+      },
+    });
+  }
+  async sendMail(to: string, subject: string, text: string, html?: string) {
+    const mailOptions = {
+      from: process.env.NODEJS_GMAIL_APP_USER, //sender address
+      to, //recipients
+      subject, //subject of the email
+      text, //plain text body of the email
+      html, //html body of the email
+    };
+
+    //send the mail
+    try {
+      const info = await this.transporter.sendMail(mailOptions);
+      console.log('Message sent: %s', info.messageId);
+    } catch (error) {
+      Logger.error(error.message);
+      throw new Error('Failed to send email');
+    }
+  }
+}
