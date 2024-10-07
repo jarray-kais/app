37e3e33f8a8cc73f0ff62073965cbc4fec66db09
jarray-kais <jarraykais1@gmail.com>
2024-10-07 08:07:55 +0100

    add timestamps option in mongoose schema
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- src/auth/schemas/user.schema.ts
+++ src/auth/schemas/user.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-@Schema()
+@Schema({ timestamps: true })
 export class User extends Document {
   @Prop({ required: true })
   name: string;
