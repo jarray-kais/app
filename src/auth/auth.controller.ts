b923657d1b0c21367d360e9060f1392a902eba1c
jarray-kais <jarraykais1@gmail.com>
2024-10-07 09:26:26 +0100

    ajouter route reset password
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Post, Body, UseGuards, Req, Put } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Req,
+  Put,
+  Param,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { signupDto } from './dto/user.dto';
 import { loginDto } from './dto/login.dto';
 import { AuthGuard } from './guards/auth.guards';
 import { ChangePasswordDto } from './dto/changePassword.dto';
 import { forgotPasswordDto } from './dto/forgot.dto';
+import { resetPasswordDto } from './dto/resetpassword.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -42,5 +51,14 @@ export class AuthController {
   @Post('/forgot')
   async forgotPasssword(@Body() forgotPasswordDto: forgotPasswordDto) {
     return this.authService.forgotPassword(forgotPasswordDto.email);
+  }
+
+  // PUT /resetpassword
+  @Put('/reset/:token')
+  async resetPassword(
+    @Param('token') token: string,
+    @Body() resetPasswordDto: resetPasswordDto,
+  ) {
+    return this.authService.resetPassword(token, resetPasswordDto.newPassword);
   }
 }
